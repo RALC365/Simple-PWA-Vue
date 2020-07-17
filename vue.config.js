5de26e065d86a2952d7596b63df762b73b6ac5f3
RALC365 <alejandro.carcamo@unitec.edu>
2020-07-17 00:27:25 -0600

    Firebase push notifications implemented

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
-module.exports = {
+/*module.exports = {
   "transpileDependencies": [
     "vuetify"
   ]
+}
+*/
+module.exports = {
+  pwa: {
+      // configure the workbox plugin
+      workboxPluginMode: 'InjectManifest',
+      workboxOptions: {
+        swSrc: "src/firebase-messaging-sw.js"
+      }
+    }
 }
